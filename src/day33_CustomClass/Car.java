0b9c342df403e0e66741037d2f992457f15f025c
Osman Yardimci <yardimciosman@gmail.com>
2020-05-04 11:13:52 -0500

    Car

---
+++ src/day33_CustomClass/Car.java
@@ -0,0 +1,12 @@
+package day33_CustomClass;
+
+public class Car {
+    String Brand; //instance variables: object variables, decleard out side of the blocks, it is not initiated
+    String model;
+    int year;
+    String color;
+    public static void main(String[] args) {
+        int a=100; //local veriable: variables that are declared within a block
+
+    }
+}
