a96f7aac2962564ba8bcb1bb615bb18ffb666ba5
Osman Yardimci <yardimciosman@gmail.com>
2020-05-13 13:27:10 -0500

    instances

---
+++ src/OfficeHours/Practice_05_13_2020/InstanceTest.java
@@ -0,0 +1,23 @@
+package OfficeHours.Practice_05_13_2020;
+
+public class InstanceTest {
+    public static void main(String[] args) {
+        Instances obj1=new Instances();
+        obj1.name="Muhtar";
+
+        Instances obj2=new Instances();
+        obj2.name="Kuzzat";
+
+        System.out.println(obj1.name); //Muhtar
+        System.out.println(obj2.name); //Kuzzat
+
+    }
+
+    static class  InstanceBlock{
+
+        public static void main(String[] args) {
+            Instances obj=new Instances();
+            System.out.println(obj.name);
+        }
+    }
+}
